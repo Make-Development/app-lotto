9c5bf4c983c1c044780a93d3bf08b30e33a95026
Make-Development <phutthithon@gmail.com>
2020-12-16 22:28:28 +0700

    test1

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/app/assets')));
+// app.use(express.static(path.join(__dirname, '/app/assets')));
 
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/view/login.html'));
+// app.get('/', (req, res, next) => {
+//     res.sendFile(path.join(__dirname + '/view/login.html'));
+// });
+
+// app.get('/auth', (req, res, next) => {
+//     res.sendFile(path.join(__dirname + '/view/auth.html'));
+// });
+
+// require('./app/routes/index.routes')(app);
+app.get('/', (req, res) => {
+    res.json({result:"ok",data:[1,2,3,4,5]});
 });
-
-app.get('/auth', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/view/auth.html'));
-});
-
-require('./app/routes/index.routes')(app);
 
 app.listen(PORT, () => {
     console.log('sever', PORT);
